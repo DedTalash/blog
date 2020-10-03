2f0a3f31df30d9063eeac7bc42b9e0588414f415
aliak <zaysh1111@gmail.com>
2020-10-03 19:05:19 +0300

    middle of smth+material ui

--- src/view/MainPage.tsx
+++ src/view/MainPage.tsx
@@ -1,19 +1,109 @@
 import React from "react";
-import {LinearProgress} from "@material-ui/core";
+import {AppBar, Button, Fab, IconButton, LinearProgress,  Typography} from "@material-ui/core";
 import "./main.css";
 import PostView from "./PostView";
 import {connect} from "react-redux";
 import {BlogReducers} from "../redux/store";
 import Post from "../model/Post";
+import {makeStyles} from "@material-ui/core/styles";
+import MenuIcon from '@material-ui/icons/Menu';
+import Toolbar from "@material-ui/core/Toolbar";
+import useScrollTrigger from "@material-ui/core/useScrollTrigger";
+import Zoom from "@material-ui/core/Zoom";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Container from "@material-ui/core/Container";
+import Box from "@material-ui/core/Box";
+import classes from "*.module.css";
+import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp'
+import PropTypes from 'prop-types';
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+        position: 'fixed',
+        bottom: theme.spacing(2),
+        right: theme.spacing(2),
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+    },
+
+}));
+
+function ScrollTop(props:any) {
+    const { children, window } = props;
+    const classes = useStyles();
+    // Note that you normally won't need to set the window ref as useScrollTrigger
+    // will default to window.
+    // This is only being set here because the demo is in an iframe.
+    const trigger = useScrollTrigger({
+        target: window ? window() : undefined,
+        disableHysteresis: true,
+        threshold: 100,
+    });
+
+    const handleClick = (event:any) => {
+        const anchor = (event.target.ownerDocument || document).querySelector('#back-to-top-anchor');
+
+        if (anchor) {
+            anchor.scrollIntoView({ behavior: 'smooth', block: 'center' });
+        }
+    };
+
+    return (
+        <Zoom in={trigger}>
+            <div onClick={handleClick} role="presentation" className={classes.root}>
+                {children}
+            </div>
+        </Zoom>
+    );
+}
+
+ScrollTop.propTypes = {
+    children: PropTypes.element.isRequired,
+    /**
+     * Injected by the documentation to work in an iframe.
+     * You won't need it on your project.
+     */
+    window: PropTypes.func,
+};
 const MainPage = (props: {posts: Post[], processing: boolean, width?:number}) =>
+
 {
+    const classes = useStyles();
     return <React.Fragment>
-        <h1>Blog</h1>
-        {props.posts.map(post => <PostView post={post} key={post.url}/>)}
-        {props.processing && <div className="line">
-            <LinearProgress color="secondary"/>
-        </div>}
+        <CssBaseline />
+        <AppBar >
+            <Toolbar>
+                <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                    <MenuIcon />
+                </IconButton>
+                <Typography variant="h6" className={classes.title}>
+                    Blog
+                </Typography>
+                <Button color="inherit">Login</Button>
+            </Toolbar>
+        </AppBar>
+        <Toolbar id="back-to-top-anchor" />
+        <Container>
+            {props.posts.map(post => <PostView post={post} key={post.url}/>)}
+            {props.processing && <div className="line">
+                <LinearProgress color="secondary"/>
+            </div>}
+        </Container>
+        <ScrollTop {...props}>
+            <Fab color="secondary" size="small" aria-label="scroll back to top">
+                <KeyboardArrowUpIcon />
+            </Fab>
+        </ScrollTop>
+        {/*<div className={classes.root}>*/}
+        {/*    <AppBar position="static"  >*/}
+                       {/*    </AppBar>*/}
+        {/*</div>*/}
+
+
     </React.Fragment>;
 }
 
--- src/view/PostView.tsx
+++ src/view/PostView.tsx
@@ -1,54 +1,101 @@
 import React from "react";
 import Post from "../model/Post";
 import {Formatter} from "../components/Formatter";
-import {Card, CardContent, CardMedia, Typography} from "@material-ui/core";
+import {Card, CardContent, CardMedia, createMuiTheme,  Typography} from "@material-ui/core";
 import {makeStyles} from "@material-ui/core/styles";
-import {useSelector} from "react-redux";
-import {BlogReducers} from "../redux/store";
 
 interface Props {
     post: Post
     width?: number
 }
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        display: 'flex',
-    },
-    cover: {
-         flex: '0 0 200px'
-
-    },
-    title: {
-        [theme.breakpoints.down('sm')]: {"font-size": '10px'},
-        [theme.breakpoints.down('md')]: {"font-size": '1.2rem'},
-        [theme.breakpoints.up('md')]:  {"font-size": '1rem'}
-    },
-    bodyPost:{
-        [theme.breakpoints.down('sm')]: {fontsize: '10px'},
-        [theme.breakpoints.down('md')]: {fontsize: '2.2rem'},
-        [theme.breakpoints.up('md')]:  {fontsize: '1rem'}
+const myTheme:any = createMuiTheme({
+    breakpoints: {
+        values: {
+            xs: 0,
+            sm: 384,
+            md: 560,
+            lg: 674,
+            xl: 1200
+        }
     }
-}));
+});
 
-// const useStylesMedium = makeStyles((theme) => ({
-//     root: {
-//         display: 'flex',
-//     },
-//     cover: {
-//         flex: '0 0 100px'
-//     }
-// }));
+const useStyles =  makeStyles((theme) => {
+
+        theme.breakpoints = myTheme.breakpoints;
+
+    console.log([theme.breakpoints.down('xs')])
+    console.log([theme.breakpoints.up('xs')])
+    console.log([theme.breakpoints.down('sm')])
+    console.log([theme.breakpoints.up('sm')])
+    return ({
+            root: {
+                display: 'flex'
+            },
+            blogPost: {
+                display: 'flex',
+                [theme.breakpoints.down('xs')]: {
+                    display: 'block',
+                },
+                [theme.breakpoints.down('sm')]:{
+                    display: 'block',
+                },
+            },
+            cover: {
+                flex: '0 0 200px',
+                height: "110px",
+                paddingTop: '25px',
+                backgroundSize: 'contain',
+                [theme.breakpoints.down('xs')]: {
+                    display:'block',
+                    height: '171px',
+                    width: '100%',
+                    // objectFit:'cover',
+                    backgroundSize: 'contain'
+                },
+                [theme.breakpoints.down('sm')]: {
+                    display: 'block',
+                    width: '100%',
+                    height: '250px',
+                    backgroundSize: 'cover'
+                },
+                [theme.breakpoints.down('md')]:{
+                    flex: '0 0 273px',
+                    paddingTop: '65px',
+                    backgroundSize: 'contain'
+                }
+            },
+            title: {
+                fontSize: '18px',
+                [theme.breakpoints.down('md')]: {fontSize: '16px', color: 'blue'},
+                [theme.breakpoints.down('sm')]: {fontSize: '16px', color: 'green'},
+                [theme.breakpoints.down('xs')]: {fontSize: '16px', color: 'red'},
+
+            },
+            bodyPost: {
+                fontSize: '16px',
+                [theme.breakpoints.down('sm')]: {fontSize: '14px', color:'red'},
+                [theme.breakpoints.down('md')]: {fontSize: '14px', color: 'blue'},
+                [theme.breakpoints.down('xs')]: {fontSize: '14px', color: 'red'}
+            },
+            date: {
+                [theme.breakpoints.down('sm')]: {fontSize: '10px', color:'red'},
+                [theme.breakpoints.down('md')]: {fontSize: '10px', color: 'blue'},
+                [theme.breakpoints.down('xs')]: {fontSize: '10px', color: 'red'},
+            }
+        }
+    );
+});
+
+
 
 
 export default function PostView(props: Props) {
     const classes = useStyles();
-    // const width: number = useSelector((state: BlogReducers) => state.width);
-    // if(width < 900) {
-    //     const classes = useStylesMedium();
-    // }
+
     return (
-        <Card className={classes.root + ' blog-post'}>
+        <Card className={classes.blogPost} >
             <CardMedia
                 className={classes.cover}
                 image={props.post.urlToImage}
@@ -58,10 +105,10 @@ export default function PostView(props: Props) {
                 <Typography className={classes.title} component="h5" variant="h5" gutterBottom={true}>
                     {props.post.title}
                 </Typography>
-                <Typography className={"bodyPost"} variant="body1" color="textSecondary" gutterBottom={true}>
+                <Typography className={classes.bodyPost} variant="body1" color="textSecondary" gutterBottom={true}>
                     {props.post.description}
                 </Typography>
-                <Typography variant={"body2"} color="textSecondary" align={"right"}>
+                <Typography className={classes.date} variant={"body2"} color="textSecondary" align={"right"}>
                     {Formatter.toDate(props.post.publishedAt)}
                 </Typography>
             </CardContent>
